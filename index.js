284ac2f19233ce8b4c2199caa57b97438b47a169
dy <df.creative@gmail.com>
2019-03-16 17:40:13 -0400

    Fix buffer creation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 module.exports = function (val, opts) {
 	opts = opts || {};
 
-	var base64 = new Buffer(typeof val === 'string' ? val : JSON.stringify(val)).toString('base64');
+	var base64 = Buffer.from(typeof val === 'string' ? val : JSON.stringify(val)).toString('base64');
 	var contents = 'sourceMappingURL=data:application/json;base64,' + base64;
 
 	if (opts.type === 'css') {
