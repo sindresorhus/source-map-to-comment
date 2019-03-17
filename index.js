3c13bd8a46a9a766cb7f51455a76b5ee3ca2dc5f
dy <df.creative@gmail.com>
2019-03-17 09:39:35 -0400

    Fix xo/travis

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 module.exports = function (val, opts) {
 	opts = opts || {};
 
-	var base64 = Buffer.from(typeof val === 'string' ? val : JSON.stringify(val)).toString('base64');
-	var contents = 'sourceMappingURL=data:application/json;base64,' + base64;
+	const base64 = Buffer.from(typeof val === 'string' ? val : JSON.stringify(val)).toString('base64');
+	const contents = 'sourceMappingURL=data:application/json;base64,' + base64;
 
 	if (opts.type === 'css') {
 		return '/*# ' + contents + ' */';
